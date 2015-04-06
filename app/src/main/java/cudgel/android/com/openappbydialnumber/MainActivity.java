00dddde2e95acec1544675fc5c1739572f21edeb
Krishna Patel <softeng.krishna@gmail.com>
2015-04-06 11:33:43 +0530

    fg

--- app/src/main/java/cudgel/android/com/openappbydialnumber/MainActivity.java
+++ app/src/main/java/cudgel/android/com/openappbydialnumber/MainActivity.java
@@ -1,17 +1,35 @@
 package cudgel.android.com.openappbydialnumber;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+Button btnhide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnhide = (Button)findViewById(R.id.btnhide);
+        btnhide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PackageManager p = getPackageManager();
+                ComponentName componentName = new ComponentName(MainActivity.this, MainActivity.class); // activity which is first time open in manifiest file which is declare as <category android:name="android.intent.category.LAUNCHER" />
+                p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+            }
+        });
+
+
+
     }
 
 
--- app/src/main/java/cudgel/android/com/openappbydialnumber/MyOutgoingCallHandler.java
+++ app/src/main/java/cudgel/android/com/openappbydialnumber/MyOutgoingCallHandler.java
@@ -1,8 +1,10 @@
 package cudgel.android.com.openappbydialnumber;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,13 +14,15 @@ import android.widget.Toast;
 public class MyOutgoingCallHandler extends BroadcastReceiver {
 
 
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
     String phoneNumber = getResultData();
     if (phoneNumber == null) {
 
-       
+
         // No reformatted number, use the original
       //  phoneNumber = Intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
     }
@@ -27,11 +31,18 @@ public class MyOutgoingCallHandler extends BroadcastReceiver {
         // My app will bring up, so cancel the broadcast
         setResultData(null);
 
+
+        PackageManager p = context.getPackageManager();
+        ComponentName componentName = new ComponentName(context, cudgel.android.com.openappbydialnumber.MainActivity.class);
+        p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
         // Start my app
         Intent i=new Intent(context,MainActivity.class);
         i.putExtra("extra_phone", phoneNumber);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
+
+
     }
 
     } // Extract phone number reformatted by previous receivers
